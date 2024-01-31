aead712258bb0623761964221328518e52807942
Dickson <mukweesamai@gmail.com>
2024-01-31 14:04:15 +0300

    modified:   src/stock_manager_backend/src/lib.rs

--- src/stock_manager_backend/src/lib.rs
+++ src/stock_manager_backend/src/lib.rs
@@ -1,4 +1,10 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+#[macro_use]
+extern crate serde;
+use candid::{Decode, Encode};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{BoundedStorable, Cell, DefaultMemoryImpl, StableBTreeMap, Storable};
+use std::{borrow::Cow, cell::RefCell};
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+type IdCell = Cell<u64, Memory>;
+
